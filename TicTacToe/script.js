a22102ebbf026716351380153b476396c5ab8490
Elodie Mai <117292826+elodiemai@users.noreply.github.com>
2023-02-02 17:08:14 -0500

    Add files via upload

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,159 +1,223 @@
-const Player = (name, symbol) => {
-    const getInfoPlayer = () => { return `${name} is playing ${symbol}.` };
-    return {
-        name,
-        getInfoPlayer,
-        symbol
-    }
-}
+'use strict';
 
-let user = Player('Human', 'O');
-let comp = Player('Computer', 'X');
+(function () {
+    const gameController = (() => {
 
+        /**
+         * Represents a Player
+         * @param {*} name - the name of the player, default values either 'Human' or 'Computer'
+         * @param {*} symbol - the symbol of the player, default values either 'X or 'O'
+         */
 
-
-const Gameboard = ((player1, player2) => {
-    let _game = [];
-    let _moveLog = {};
-    let _victory = false;
-    let _playerStart = (Math.round(Math.random())) ? player1 : player2;
-    _moveLog[player1.name] = 0;
-    _moveLog[player2.name] = 0;
-
-    resetGame();
-
-    function resetGame() {
-        _game = [];
-        let row = [];
-        for (let i = 0; i < 3; i++) {
-            row.push('');
-        }
-        for (let i = 0; i < 3; i++) {
-            _game.push(row);
-        }
-        _victory = false;
-        document.querySelectorAll('.cell').forEach(cell => {
-            cell.innerText = ''
-            cell.classList.remove('O');
-            cell.classList.remove('X');
-        }
-        );
-    }
-
-    function addMark(player, position) {
-        const [row, col] = position.split(',');
-        if (_isLegalMove(row, col) && whosTurn() === player && !_victory) {
-            const updatedRow = [..._game[row - 1]].map((val, j) => (j === col - 1) ? player.symbol : val);
-            _game.splice(row - 1, 1, updatedRow);
-            _moveLog[player.name] += 1;
-            if (checkIfVictory(player)) {
-                _printMsg(`${player.name} won!`);
-                document.querySelector('.button').innerHTML = '<p>Play again</p>';
-                document.querySelector('.button').classList.toggle('deactivate');
+        const Player = (name, symbol) => {
+            const getInfoPlayer = () => (`${name} is playing ${symbol}.`);
+            return {
+                name,
+                getInfoPlayer,
+                symbol
             }
-            return true;
         }
-        _checkEndGame();
-    }
 
-    function _checkEndGame() {
-        if (_game.every(row => row.every(cell => cell !== '')) && !_victory)  {
+        const Gameboard = (() => {
+            let game = [];
+            let moveLog = {};
+            return {
+                get moveLog() {
+                    return moveLog;
+                },
+                set moveLog(obj) {
+                    moveLog = obj;
+                },
+                get game() {
+                    return game;
+                },
+                set game(arr) {
+                    game = arr;
+                },
+            }
+        })();
+
+
+        const user = Player('Human', 'O');
+        const comp = Player('Computer', 'X');
+        let _victory = false;
+        let _playerStart = (Math.round(Math.random())) ? user : comp;
+
+        /**
+         * Initializes the game log to 0 move for each player
+         */
+        const initMoveLog = () => {
+            let _log = {};
+            _log[user.name] = 0;
+            _log[comp.name] = 0;
+            Gameboard.moveLog = _log;
+        };
+
+        /**
+         * Resets the game array to [['','','']['','','']['','','']]
+         */
+        const initGame = () => {
+            Gameboard.game = [];
+            let row = [];
+            for (let i = 0; i < 3; i++) {
+                row.push('');
+            }
+            for (let i = 0; i < 3; i++) {
+                Gameboard.game.push(row);
+            }
+        };
+
+
+        const resetGame = () => {
+            initMoveLog();
+            initGame();
+            _victory = false;
+            displayController.resetDisplayGame();
+            if (!doesPlayerStart()) compMove();
+        };
+
+        const playMove = (player, position) => {
+            const [row, col] = position.split(',');
+            if (_isLegalMove(row, col) && whosTurn() === player && !_victory) {
+                const updatedRow = [...Gameboard.game[row - 1]].map((val, j) => (j === col - 1) ? player.symbol : val);
+                Gameboard.game.splice(row - 1, 1, updatedRow);
+                Gameboard.moveLog[player.name] += 1;
+                if (checkIfVictory(player)) {
+                    _printGameWin(player);
+                }
+                return true;
+            }
+            _checkEndGame();
+        }
+
+
+        const _checkEndGame = () => {
+            if (Gameboard.game.every(row => row.every(cell => cell !== '')) && Gameboard.game.length > 0 && !_victory) {
+                _printGameTie();
+            }
+        }
+
+        const checkIfVictory = (player) => {
+            let isVictory = false;
+            //check rows
+            Gameboard.game.forEach(row => {
+                if (row.every(cell => cell === player.symbol)) {
+                    isVictory = true;
+                }
+            })
+            //check columns
+            for (let i = 0; i <= 2; i++) {
+                if (Gameboard.game.map(row => row[i]).every(cell => cell === player.symbol)) {
+                    isVictory = true;
+                }
+            }
+            //check diagonals
+            if (Gameboard.game[0][0] === player.symbol && Gameboard.game[1][1] === player.symbol && Gameboard.game[2][2] === player.symbol) {
+                isVictory = true;
+            }
+            if (Gameboard.game[2][0] === player.symbol && Gameboard.game[1][1] === player.symbol && Gameboard.game[0][2] === player.symbol) {
+                isVictory = true;
+            }
+            _victory = isVictory;
+            return isVictory;
+        }
+
+        const whosTurn = () => {
+            if (user === _playerStart) {
+                return (Gameboard.moveLog[user.name] > Gameboard.moveLog[comp.name]) ? comp : user;
+            } else {
+                return (Gameboard.moveLog[comp.name] > Gameboard.moveLog[user.name]) ? user : comp;
+            }
+        }
+
+        const compMove = () => {
+            if (!_victory && Gameboard.game.some(row => row.some(cell => cell === ''))) {
+                let position = '';
+                while (position === '') {
+                    let i = Math.floor(Math.random() * 3 + 1);
+                    let j = Math.floor(Math.random() * 3 + 1);
+                    position = (_isLegalMove(i, j)) ? `${i},${j}` : '';
+                }
+                if (playMove(comp, position)) {
+                    displayController.addMarkDisplay(comp, position);
+                }
+            }
+            _checkEndGame();
+        }
+
+        const doesPlayerStart = () => (_playerStart === user);
+
+        const _isLegalMove = (i, j) => (Gameboard.game[i - 1][j - 1] === '');
+
+        const _printGameWin = (player) => {
+            _printMsg(`${player.name} won!`);
+            document.querySelector('.button').innerHTML = '<p>Play again</p>';
+            document.querySelector('.button').classList.toggle('deactivate');
+        }
+
+        const _printGameTie = () => {
             _printMsg('Oh. It\'s a tie!');
             document.querySelector('.button').innerHTML = '<p>Play again</p>';
             document.querySelector('.button').classList.toggle('deactivate');
         }
-    }
 
-    function checkIfVictory(player) {
-        let isVictory = false;
-        //check rows
-        _game.forEach(row => {
-            if (row.every(cell => cell === player.symbol)) {
-                isVictory = true;
+        const _printMsg = (message) => document.querySelector('.message').innerText = message;
+
+
+        return {
+            playMove,
+            resetGame,
+            compMove,
+            doesPlayerStart,
+            checkIfVictory,
+            whosTurn
+        }
+    })();
+
+    const displayController = (() => {
+
+        const startGame = () => {
+            gameController.resetGame();
+            const startMessage = (gameController.doesPlayerStart()) ? 'You start.' : 'Computer starts.';
+            document.querySelector('.message').innerText = startMessage;
+            document.querySelector('.button').classList.toggle('deactivate');
+            document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', eventMove));
+        };
+
+
+
+
+        const resetDisplayGame = () => {
+            document.querySelectorAll('.cell').forEach(cell => {
+                cell.innerText = ''
+                cell.classList.remove('O');
+                cell.classList.remove('X');
+            })
+        }
+
+        const addMarkDisplay = (player, position) => {
+            document.querySelector(`.cell[data-attribute="${position}"]`).innerText = player.symbol;
+            document.querySelector(`.cell[data-attribute="${position}"]`).classList.toggle(player.symbol);
+        }
+
+        document.querySelector('.button').addEventListener('click', startGame);
+
+        const eventMove = (event) => {
+            const player = gameController.whosTurn();
+            if (gameController.playMove(player, event.target.dataset.attribute)) {
+                event.target.innerText = player.symbol;
+                event.target.classList.toggle(player.symbol);
+                gameController.compMove();
             }
-        })
-        //check columns
-        for (let i = 0; i <= 2; i++) {
-            if (_game.map(row => row[i]).every(cell => cell === player.symbol)) {
-                isVictory = true;
-            }
         }
-        //check diagonals
-        if (_game[0][0] === player.symbol && _game[1][1] === player.symbol && _game[2][2] === player.symbol) {
-            isVictory = true;
+
+        return {
+            addMarkDisplay,
+            resetDisplayGame
         }
-        if (_game[2][0] === player.symbol && _game[1][1] === player.symbol && _game[0][2] === player.symbol) {
-            isVictory = true;
-        }
-        _victory = isVictory;
-        return isVictory;
-    }
 
-    function whosTurn() {
-        if (player1 === _playerStart) {
-            return (_moveLog[player1.name] > _moveLog[player2.name]) ? player2 : player1;
-        } else {
-            return (_moveLog[player2.name] > _moveLog[player1.name]) ? player1 : player2;
-        }
-    }
+    })();
 
-    function compMove() {
-        if (!_victory && _game.some(row => row.some(cell => cell === ''))) {
-            let position = '';
-            while (position === '') {
-                let i = Math.floor(Math.random() * 3 + 1);
-                let j = Math.floor(Math.random() * 3 + 1);
-                position = (_isLegalMove(i, j)) ? `${i},${j}` : '';
-            }
-            if (addMark(player2, position)) {
-                document.querySelector(`.cell[data-attribute="${position}"]`).innerText = player2.symbol;
-                document.querySelector(`.cell[data-attribute="${position}"]`).classList.toggle(player2.symbol);
-            }
-        }
-        _checkEndGame();
-    }
-
-    const doesPlayerStart = () => (_playerStart === user);
-    const _printMsg = (message) => document.querySelector('.message').innerText = message;
-    const _isLegalMove = (i, j) => (_game[i - 1][j - 1] === '');
-
-
-    document.querySelector('.button').addEventListener('click', printWhoStarts);
-
-
-    function playMove(event) {
-        const player = Gameboard.whosTurn();
-        if (Gameboard.addMark(player, event.target.dataset.attribute)) {
-            event.target.innerText = player.symbol;
-            event.target.classList.toggle(player.symbol);
-            Gameboard.compMove();
-        }
-    }
-
-    function printWhoStarts() {
-        Gameboard.resetGame();
-        const startMessage = (Gameboard.doesPlayerStart()) ? 'You start.' : 'Computer starts.';
-        document.querySelector('.message').innerText = startMessage;
-        document.querySelector('.button').classList.toggle('deactivate');
-        if (!Gameboard.doesPlayerStart()) Gameboard.compMove();
-        document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', playMove));
-    };
-
-
-    return {
-        _game,
-        resetGame,
-        compMove,
-        doesPlayerStart,
-        addMark,
-        checkIfVictory,
-        whosTurn
-    }
-
-
-})(user, comp)
-
-
-
+})();
 
 
